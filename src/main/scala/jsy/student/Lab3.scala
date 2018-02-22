3855b237c0a9de8520184cc9b02ecfa25255b63d
Ian Smith <papachingching@gmail.com>
2018-02-22 10:40:09 -0700

    fixed BigStep AND
    made sure that each expression in binary AND is only evaluated once

--- src/main/scala/jsy/student/Lab3.scala
+++ src/main/scala/jsy/student/Lab3.scala
@@ -145,7 +145,11 @@ object Lab3 extends JsyApplication with Lab3Like {
 
         /* Binary Comparison Ops */
         // return first to eval to false or if both are true return the first expr
-        case And => if(!toBoolean(eval(env,e1))) eval(env,e1) else eval(env,e2)
+        case And => //if(!toBoolean(eval(env,e1))) eval(env,e1) else eval(env,e2)
+          {
+            val eval1 = eval(env, e1)
+            if (toBoolean(eval1)) eval(env,e2) else eval1
+          }
         // return the first to eval to true, if both false return the 2nd expr
         case Or => if(toBoolean(eval(env,e1))) eval(env,e1) else eval(env,e2)
         case Eq => (eval(env,e1),eval(env,e2)) match {
